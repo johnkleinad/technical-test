bfa9588b478e75a93c80809f98868dee0dcfb49d
juanito <juan_mejia13@outlook.com>
2022-04-16 16:49:06 -0500

    add theme button

--- layout/Main.jsx
+++ layout/Main.jsx
@@ -2,18 +2,24 @@ import SearchBar from "../components/SearchBar";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { FadeIn } from "../components/Animate";
+import { IoMoon, IoSunny } from "react-icons/io5";
 
-const MainLayout = ({ children, title }) => {
+const MainLayout = ({ children, title, modalOpen }) => {
     const [isOnFocus, setIsOnFocus] = useState(false);
     return <>
         <motion.section
-            animate={{opacity:[0, 1]}}
-            className="fixed inset-0 bg-gray-scale-10">
+            animate={{scale: modalOpen ? 0.91 : 1 }}
+            className={`${modalOpen && 'rounded-t-lg overflow-hidden'} fixed inset-0 bg-gray-scale-10`}>
             <motion.div
                 animate={{ opacity: isOnFocus ? 0 : 1, y: isOnFocus ? -100 : 0, height: isOnFocus ? 0 : 70 }}
                 transition={{ duration: 0.3, type: 'easeInOut' }}
-                className="sticky px-4 py-2 top-0">
+                className="sticky px-4 py-2 top-0 center-y justify-between">
                 <span className="text-5xl font-bold">{title}</span>
+                <button>
+                    <FadeIn show={true}>
+                        <IoSunny size={25} />
+                    </FadeIn>
+                </button>
             </motion.div>
             <div className="overflow-y-auto h-full">
                 <SearchBar isOnFocus={isOnFocus} setIsOnFocus={setIsOnFocus} />
