331cddef5c80d5302c8718a6ebe98e1f525f38b8
juanito <juan_mejia13@outlook.com>
2022-04-16 16:51:06 -0500

    fixed modal

--- components/UserInfoCard.jsx
+++ components/UserInfoCard.jsx
@@ -3,10 +3,10 @@ import { FadeInModal } from './Animate';
 import ModalOptions from './ModalOptions';
 import { IoCopy, IoCreateOutline } from "react-icons/io5";
 
-const UserInfoCard = ({ data }) => {
+const UserInfoCard = ({ data, getUser }) => {
     const { firstName, secondName, familyName, lastName, email, cel, birthday, assignedAnalyst, id } = data
     const [isOpenCard, setIsOpenCard] = useState();
-    const [showStatus, setShowStatus] = useState();
+    const [showStatus, setShowStatus] = useState(false);
     const [userStatus, setUserStatus] = useState(data.status);
     const userId = createRef();
     const copyToClipboard = (msg) => {
@@ -20,14 +20,14 @@ const UserInfoCard = ({ data }) => {
         { name: 'EN PROCESO', color: 'bg-yellow-500' },
         { name: 'COMPLETADO', color: 'bg-green-600' }
     ]
-    const colorStatus = statusOptions.find((color) => color.name == userStatus)
+    const colorStatus = statusOptions.find((status) => status.name == userStatus)
     return <>
         <div className=' w-full flex p-2 px-4 h-[250px] text-sm mb-5'>
             <div className="flex flex-col justify-between w-full">
                 <div className="flex flex-col">
                     <span className="text-primary-50 font-semibold capitalize select-none">{`${firstName} ${secondName} ${familyName} ${lastName}`}</span>
                     <button onClick={() => copyToClipboard(id)} className="text-neutral-400 center-y">
-                        <IoCopy/> <span className="mx-2">ID: <input ref={userId} disabled className='truncate text-black -z-10' value={id} /></span>
+                        <IoCopy /> <span className="mx-2">ID: <input ref={userId} className='truncate select-none focus:outline-none' value={id} /></span>
                     </button>
                 </div>
                 <div className="flex flex-col">
@@ -51,7 +51,7 @@ const UserInfoCard = ({ data }) => {
                 <div className="flex justify-between whitespace-nowrap w-full">
                     <button
                         onClick={() => setShowStatus(!showStatus)}
-                        className={`${colorStatus.color} text-white rounded px-3 py-2`}>
+                        className={`${colorStatus.color} text-white rounded px-3 py-2 w-32`}>
                         {userStatus}
                     </button>
                     <button
@@ -61,11 +61,15 @@ const UserInfoCard = ({ data }) => {
                     </button>
                 </div>
             </div>
-            <div className='w-10 center-x'>
-                <IoCreateOutline size={20}/>
-            </div>
+            <button onClick={() => getUser(data)} className='w-10 center-x'>
+                <IoCreateOutline size={20} />
+            </button>
         </div>
-        <FadeInModal show={showStatus} set={setShowStatus}>
+        <FadeInModal
+            show={showStatus}
+            set={setShowStatus}
+            className={'absolute bottom-0 inset-x-0'}
+        >
             <ModalOptions
                 message={'Agrega un status al usuario.'}
                 arrayOpiopns={statusOptions}
