8140e2d660c487450ccdc4c3bbf7999d3ad048cc
juanito <juan_mejia13@outlook.com>
2022-04-17 18:13:40 -0500

    add refetch

--- pages/home.jsx
+++ pages/home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     const [create, setCreate] = useState(false);
 
     const getData = url => fetch(url).then(r => r.json())
-    const { data } = useSWR('/api/users', getData);
+    const { data } = useSWR('/api/users', getData, { refreshInterval: 1000 });
     const getUser = (user) => {
         setUser(user)
         setEdit(true)
