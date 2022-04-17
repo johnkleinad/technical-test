8edb41fe97a48a5470ab566dabf98563bd0c78d5
juanito <juan_mejia13@outlook.com>
2022-04-17 18:05:32 -0500

    fixed update status

--- components/EditModal.jsx
+++ components/EditModal.jsx
@@ -5,39 +5,32 @@ import CustomInput from "./CustomInput"
 import { User } from '../model/user'
 import ModalOptions from "./ModalOptions";
 import Router from "next/router";
+import UseUpdateUser from "../hooks/UseUpdateUser";
 
 const EditModal = ({ setEdit, user }) => {
     const [showStatus, setShowStatus] = useState(false);
     const [userStatus, setUserStatus] = useState(user.status);
     const [message, setMessage] = useState();
-    const statusOptions = [
-        { name: 'PENDIENTE', color: 'bg-neutral-400' },
-        { name: 'EN PROCESO', color: 'bg-yellow-500' },
-        { name: 'COMPLETADO', color: 'bg-green-600' }
-    ]
+    const update = UseUpdateUser()
+    const statusOptions = {
+        'PENDIENTE': 'bg-neutral-400',
+        'EN PROCESO': 'bg-yellow-500',
+        'COMPLETADO': 'bg-green-600'
+    }
     const userInputs = Object.entries(user).map((uInput) => {
         return {
             name: uInput[0],
             input: UseFromInputs(uInput[1])
         }
     })
-    const submitEdit = async () => {
+    const submitEdit =() => {
         if (userInputs.some((userInput) => userInput.input.value == '')) return console.log('Necestia todos los parametros');
         const body = new User({value:userInputs.map((userInput) => userInput), status:userStatus});
-        console.log(body);
-        const response = await fetch('/api/users', {
-            method: 'PUT',
-            body: JSON.stringify(body)
-        })
-        if (!response.ok) return ('error del servidor')
-        const data = await response.json()
-        console.log(data);
-        if (data.message == 'ok') {
+        update.submitUpdate(body)
+        console.log(update.res)
+        setTimeout(() => {
             setEdit(false)
-            // setTimeout(() => {
-            //     Router.reload('/home')
-            // }, 800);
-        }
+        }, 1000);
     }
     const deleteUser = (id) => {
         fetch('/api/users', {
@@ -51,7 +44,6 @@ const EditModal = ({ setEdit, user }) => {
             Router.reload('/home')
         }, 800);
     }
-    const colorStatus = statusOptions.find((status) => status.name == userStatus)
     return <>
         <div className="flex relative justify-between h-12 dark:text-white">
             <button onClick={() => setEdit(false)}>Cancelar</button>
@@ -72,7 +64,7 @@ const EditModal = ({ setEdit, user }) => {
             })}
             <button
                 onClick={() => setShowStatus(!showStatus)}
-                className={`${colorStatus.color} text-white rounded px-3 py-2 mt-2 w-full`}>
+                className={`${statusOptions[userStatus]} text-white rounded px-3 py-2 mt-2 w-full`}>
                 {userStatus}
             </button>
             <button
@@ -91,7 +83,7 @@ const EditModal = ({ setEdit, user }) => {
         >
             <ModalOptions
                 message={'Agrega un status al usuario.'}
-                arrayOpiopns={statusOptions}
+                arrayOpiopns={Object.entries(statusOptions)}
                 selection={setUserStatus}
                 close={setShowStatus}
             />
