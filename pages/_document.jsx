b0e0e93cb37f5919fc1a0f4e66142f0a2567f9c9
juanito <juan_mejia13@outlook.com>
2022-04-17 14:26:15 -0500

    add meta description

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,8 @@ const Document = () => {
     return (
         <Html lang="es">
             <Head>
+                <title>Technical Test</title>
+                <meta name="description" content="Techincal Test for Atrato" />
                 <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' ></meta>
             </Head>
             <body className='dark'>
