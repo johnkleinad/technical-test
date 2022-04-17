5238dcbd0dec57811ef907fa1c3d0bc65117d516
juanito <juan_mejia13@outlook.com>
2022-04-17 04:06:04 -0500

    add dark mode conditions

--- components/CustomInput.jsx
+++ components/CustomInput.jsx
@@ -2,7 +2,7 @@ const CustomInput = ({ value, name, type = 'text' }) => {
     return <>
         <label className="text-neutral-600 dark:text-neutral-200">{name}</label>
         <input
-            className="w-full h-10 mb-2 px-2 focus:outline-none dark:bg-neutral-800 dark:text-white"
+            className="w-full rounded h-10 mb-2 px-2 focus:outline-none dark:bg-neutral-800 dark:text-white"
             {...value}
             type={type}
         />
