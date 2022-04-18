97e103e895f31b6c5442d0b07b825d3fdc824d3d
juanito <juan_mejia13@outlook.com>
2022-04-18 02:46:46 -0500

    fixed hook useUpdate

--- hooks/UseUpdateUser.jsx
+++ hooks/UseUpdateUser.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react"
-import useSWR, { useSWRConfig } from 'swr'
+import { useSWRConfig } from 'swr'
 
 const UseUpdateUser = () => {
-    const [res, serRes] = useState()
-    const { mutate } = useSWRConfig()
+    const [res, setRes] = useState({});
+    const { mutate } = useSWRConfig();
     const submitUpdate = async (body) => {
         const response = await fetch('/api/users', {
             method: 'PUT',
             body: JSON.stringify(body)
-        })
+        });
         if (!response.ok) return ('error del servidor')
         const data = await response.json()
+        if (data) {
+            setRes(data)
+        }
         mutate('/api/users')
-    }
+    };
     return {
         res,
         submitUpdate
