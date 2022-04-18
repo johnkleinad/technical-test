0de00527ed4db423f0b3f7132832ebaeda49511e
juanito <juan_mejia13@outlook.com>
2022-04-18 01:11:19 -0500

    add validations types

--- components/CustomInput.jsx
+++ components/CustomInput.jsx
@@ -1,10 +1,14 @@
-const CustomInput = ({ value, name, type = 'text' }) => {
+const CustomInput = ({ value, name }) => {
+    const typeInput = {
+        'telefono': 'cel',
+        'email': 'email',
+        'birthday': 'date'
+    }
     return <>
         <label className="text-neutral-600 dark:text-neutral-200">{name}</label>
-        <input
-            className="w-full rounded h-10 mb-2 px-2 focus:outline-none dark:bg-neutral-800 dark:text-white"
+        <input className="w-full rounded h-10 mb-2 px-2 focus:outline-none dark:bg-neutral-800 dark:text-white"
             {...value}
-            type={type}
+            type={!typeInput[name] ? 'text' : typeInput[name]}
         />
     </>
 }
