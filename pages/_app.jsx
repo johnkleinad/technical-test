1ff7ff3363c393adb4051e5303c9317c6dbfc4ea
juanito <juan_mejia13@outlook.com>
2022-04-18 01:39:05 -0500

    fixed meta data

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Technical Test</title>
+      <meta name="description" content="Techincal Test for Atrato" />
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' ></meta>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
     return (
         <Html lang="es">
-            <Head>
-                <title>Technical Test</title>
-                <meta name="description" content="Techincal Test for Atrato" />
-                <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' ></meta>
-            </Head>
+            <Head/>
             <body className='dark'>
                 <Main />
                 <NextScript />
