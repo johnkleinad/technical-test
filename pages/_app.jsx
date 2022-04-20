783795c2026e66d808306cdfb4b866975ae8d418
juanito <juan_mejia13@outlook.com>
2022-04-20 05:48:00 -0500

    change tittle

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
-      <title>Technical Test</title>
+      <title>Atrato Technical Test</title>
       <meta name="description" content="Techincal Test for Atrato" />
       <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' ></meta>
     </Head>
