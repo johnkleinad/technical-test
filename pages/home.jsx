268758f238a7f7e779ed804c11ab561ac80d3f0b
juanito <juan_mejia13@outlook.com>
2022-04-20 05:48:37 -0500

    fixed array names and delete useless useState

--- pages/home.jsx
+++ pages/home.jsx
@@ -9,23 +9,22 @@ import useSWR from "swr";
 
 const Home = () => {
     const [users, setUsers] = useState([]);
-    const [user, setUser] = useState([]);
     const [edit, setEdit] = useState(false);
     const [create, setCreate] = useState(false);
 
     const getData = url => fetch(url).then(r => r.json())
     const { data } = useSWR('/api/users', getData, { refreshInterval: 1000 });
     const getUser = (user) => {
-        setUser(user)
+        setUsers(user)
         setEdit(true)
     };
     return <>
         <MainLayout getUser={getUser} set={setCreate} title={'Usuarios'} modalOpen={edit || create}>
             <div className="my-2 pb-16 divide-y divide-neutral-300 dark:divide-neutral-600 md:divide-y-0 mx-4 auto-grid">
-                {data?.length > 0 ? data?.map((user, key) =>
+                {data?.length > 0 ? data?.map((users, key) =>
                     <div key={key} className="md:border md:dark:border-neutral-700 md:px-2 md:rounded-md ">
                         <UserInfoCard
-                            data={user}
+                            data={users}
                             setEdit={setEdit}
                             getUser={getUser}
                         />
@@ -40,7 +39,7 @@ const Home = () => {
             className={'fixed inset-0 top-4 bg-gray-scale-10 dark:bg-neutral-900 rounded-t-lg p-4'}
             speed={0.3}
         >
-            <EditModal setEdit={setEdit} edit={edit} user={user} />
+            <EditModal setEdit={setEdit} edit={edit} user={users} />
         </FadeInModal>
         <FadeInModal
             show={create}
